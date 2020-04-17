e2ee0f527b01eb181035f99b3acfb7fff8677648
George-Stephen <yazz777stevie@gmail.com>
2020-04-17 10:06:56 +0300

    built the test

---
+++ src/main/java/cipher.java
@@ -0,0 +1,12 @@
+public class cipher {
+    private String entry;
+
+
+    public cipher(String entry) {
+        this.entry = entry;
+    }
+
+    public String getEntry() {
+        return this.entry;
+    }
+}
---
+++ src/test/java/cipherTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class cipherTest {
+
+    @Test
+    public void new_cipher() {
+        cipher newData = new cipher("George Stephen");
+        assertEquals(true, newData instanceof cipher);
+    }
+    @Test
+    public void obtain_cipher(){
+        cipher newData = new cipher("George Stephen");
+        assertEquals("George Stephen",newData.getEntry());
+
+    }
+}
